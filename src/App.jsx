c552f03ff0401df1def55bf677e56d78d252525c
gusqkr <hyeon05@ewha.ac.kr>
2026-08-27 02:53:55 +0900

    feat: GA 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes/router";
 import LoadingScreen from "@/components/ui/LoadingScreen";
 import { ToastProvider } from "@/components/ui/ToastProvider";
 import { InstallPromptProvider } from "@/components/ui/InstallPromptProvider";
+import { initGA, trackPageView } from "@/lib/ga";
 
 function App() {
+  useEffect(() => {
+    initGA();
+    trackPageView(
+      router.state.location.pathname + router.state.location.search,
+    );
+
+    return router.subscribe((state) => {
+      trackPageView(state.location.pathname + state.location.search);
+    });
+  }, []);
+
   return (
     <ToastProvider>
       <InstallPromptProvider>
